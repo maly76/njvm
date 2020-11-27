583a4db8e80d1c9ada228b2d2737eafaede47717
mohammad <mohammad.alayoub@mni.thm.de>
2020-11-27 12:12:18 +0100

    aufgabe2

--- Aufgabe1/njvm.c
+++ Aufgabe1/njvm.c
@@ -3,6 +3,22 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdlib.h>
+//Macro
+#define MAXITEMS 100
+#define HALT 0
+#define PUSHC 1
+#define ADD 2
+#define SUB 3
+#define MUL 4
+#define DIV 5
+#define MOD 6
+#define RDINT 7
+#define WRINT 8
+#define RDCHR 9
+#define WRCHR 10
+#define IMMEDIATE(x)((x) & 0x00FFFFFF)
+#define SIGN_EXTEND(i)((i) & 0x00800000 ? (i) | 0xFF000000 : (i))
+#define OPCODE(x)((x) >> 24)
 
 // <--------------------Functions(Deklaration)----------------------->
 int pop();
@@ -12,65 +28,22 @@ int *cutInstructions(unsigned int instr);
 void calculate(char o);
 int execute(int i);
 void start(int programnumber);
-int findIndexOfInstruction(const char *ins);
-void saveInMemory(char *operation, int imediateV);
+void saveInMemory(int instruction);
 void print_step(void);
-// ------------------------------------------------------
+
+
 // ------------------------Variables--------------------->
-#define MAXITEMS 100
 int sp=0; // the stackpointer
 int stack[MAXITEMS];
-#ifndef TEST_H
-#define TEST_H
-const char *instructions[] = {"halt", "pushc", "add", "sub", "mul", "div",
-                             "mod", "rdint", "wrint", "rdchr", "wrchr"};
-#endif
-#define SIGN_EXTEND(i) ((i) & 0x00800000 ? (i) \
-| 0xFF000000 : (i))
+const char *instructions[] = {"halt", "pushc", "add", "sub", "mul", "div", "mod", "rdint", "wrint", "rdchr", "wrchr"};
 unsigned int program_memory[100];
 int counter = 0;
 int pc = 0;
-// ---------------------------------------------------------
-// <------------------------Functions(Definition)--------------------------->
-int main (int argc, char *argv[]) {
-    
-    if(strcmp(argv[1], "--help") == 0)
-    {
-        printf("usage: ./njvm [option] [option] ...\n"
-               "--prog1          select program 1 to execute\n"
-               "--prog2          select program 2 to execute\n"
-               "--prog3          select program 3 to execute\n"
-               "--version        show version and exit\n"
-               "--help           show this help and exit\n");
-        exit(0);
-    }
-    else if(strcmp(argv[1], "--version") == 0)
-    {
-        printf("Ninja Virtual Machine version 1\n");
-        exit(0);
-    }
-    else if (strcmp(argv[1], "prog1") == 0)
-    {
-        start(1);
-    }
-    else if (strcmp(argv[1], "prog2") == 0)
-    {
-        start(2);
-    }
-    else if (strcmp(argv[1], "prog3") == 0)
-    {
-        start(3);
-    }
-    else
-    {
-        printf("unknown command line argument '%s', try './njvm --help'\n", argv[1]);
-    }
-    return 0;
-}
 
-int *cutInstructions(unsigned int instr)
-{
-    int opcode = instr >> 24;
+// <------------------------Functions(Definition)--------------------------->
+
+int *cutInstructions(unsigned int instr){
+    int opcode = OPCODE(instr);
     int immediate = SIGN_EXTEND(instr & 0x00FFFFFF);
     //printf("0x%08x -> Opcode [%d] Immediate [%d]\n", instr, opcode, immediate);
     int *ret = malloc(2);
@@ -79,39 +52,28 @@ int *cutInstructions(unsigned int instr)
     return ret;
 }
 
-void calculate(char o)
-{
+void calculate(char o){
     int x = pop();
     int y = pop();
     int result;
     switch (o)
     {
-        case '+':             // add
-            result = x + y;
-            break;
-        case '-':             // sub
-            result = x - y;
-            break;
-        case '*':             // mul
-            result = x * y;
-            break;
-        case '/':             // div
-            result = x / y;
-            break;
-        case '%':             // modulo
-            result = x % y;
-            break;
-        default:
-            break;
+        case '+':result = x + y;break;
+        case '-':result = x - y;break;
+        case '*':result = x * y;break;
+        case '/':result = x / y;break;
+        case '%':result = x % y;break;
+        default: break;
     }
     push(result);
 }
 
-int execute(int i)
-{
+int execute(int i){
     int *instruc = cutInstructions(program_memory[pc]);
     int opcode = instruc[0];
     int immediate = instruc[1];
+    int tempint;
+    char tempchar;
     if (opcode == 0)
     {
         return 0;
@@ -120,73 +82,50 @@ int execute(int i)
     {
         switch (opcode)
         {
-        case 0:             //  halt
-            exit(0);
-            break;
-        case 1:            //  pushc
-            push(immediate);
-            break;
-        case 2:                                 // add
-            calculate('+');
-            break;
-        case 3:                                 // sub
-            calculate('-');
-            break;
-        case 4:                                 // mul
-            calculate('*');
-            break;
-        case 5:                                 // div
-            calculate('/');
-            break;
-        case 6:                                 // mod
-            calculate('%');
-            break;
-        case 7:                                 // rdint
-            push(immediate);
-            break;
-        case 8:                                 // wrint
-            printf("%d", pop());
-            break;
-        case 9:                                 // rdchr
-            push(immediate);
-            break;
-        case 10:                                // wrchr
-            pop();
-            break;
-        default:
-            break;
+        case 0: exit(0);break; //halt
+        case 1: push(immediate);break;//pushc
+        case 2: calculate('+');break; //add
+        case 3: calculate('-');break; //sub
+        case 4: calculate('*');break; //mul
+        case 5: calculate('/');break; //div
+        case 6: calculate('%');break; //mod
+        case 7: scanf("%d", &tempint); printf("Der Wert %d \n",tempint);push(tempint); break;// rdint
+        case 8: printf("%d", pop());break;// wrint
+        case 9: scanf("%c", &tempchar); push(tempchar); break;// rdchr
+        case 10: printf("%c", pop());;break;// wrchr
+        default: break;
         }
         return 1;
     }
 }
 
-void start(int programnumber)
-{
+void start(int programnumber){
     if (programnumber == 1)
+
     {
-        char *operationsP1[] = {"pushc", "pushc", "add", "pushc", "pushc", "sub", "mul", "wrint", "pushc", "wrchr", "halt"};
+        int operationsP1[] = {PUSHC, PUSHC, ADD, PUSHC, PUSHC, SUB, MUL, WRINT, PUSHC, WRCHR, HALT};
         int numbersP1[] = {3, 4, 0, 10, 6, 0, 0, 0, 10, 0, 0};
         for (size_t i = 0; i <= sizeof(numbersP1)/sizeof(numbersP1[0]) - 1; i++)
         {
-            saveInMemory(operationsP1[i], numbersP1[i] & 0x00FFFFFF);
+            saveInMemory((operationsP1[i] << 24) | IMMEDIATE(numbersP1[i]));
         }
     }
     else if (programnumber == 2)
     {
-        char *operationsP2[] = {"pushc", "rdint", "mul", "pushc", "add", "wrint", "pushc", "wrchr", "halt"};
+        int operationsP2[] = {PUSHC, RDINT, MUL, PUSHC, ADD, WRINT, PUSHC, WRCHR, HALT};
         int numbersP2[] = {-2, 0, 0, 3, 0, 0, 0, 0, 0};
         for (size_t j = 0; j <= sizeof(numbersP2)/sizeof(numbersP2[0]) - 1; j++)
         {
-            saveInMemory(operationsP2[j], numbersP2[j] & 0x00FFFFFF);
+            saveInMemory((operationsP2[j] << 24)  | IMMEDIATE(numbersP2[j]));
         }
     }
     else if (programnumber == 3)
     {
-        char *operationsP3[] = {"rdchr", "wrint", "pushc", "wrchr", "halt"};
+        int operationsP3[] = {RDCHR, WRINT, PUSHC, WRCHR, HALT};
         int numbersP3[] = {0, 0, 0, 0, 0};
         for (size_t m = 0; m <= sizeof(numbersP3)/sizeof(numbersP3[0]) - 1; m++)
         {
-            saveInMemory(operationsP3[m], numbersP3[m] & 0x00FFFFFF);
+          saveInMemory((operationsP3[m]  << 24)  | IMMEDIATE(numbersP3[m]));
         }
     }
 
@@ -202,8 +141,7 @@ void start(int programnumber)
     }
 }
 
-void print_step(void)
-{
+void print_step(void){
     printf("\n ASSEMBLER     |  BYTECODE  \n");
     printf(".--------------+------------------.\n");
     int i = 0;
@@ -231,38 +169,22 @@ void print_step(void)
         i++;
     }
     printf("------------------------------------\n");
-    
+
     for (int i = 0; i <= 10; i++)
     {
     }
 }
 
-int findIndexOfInstruction(const char *ins)
-{
-    int index = 0;
-    for (int i = 0; i < sizeof(instructions)/sizeof(instructions[0]); i++)
-    {
-        if (strcmp(instructions[i], ins) == 0)
-        {
-            index = i;
-        }
-    }
-    return index;
-}
-
-void saveInMemory(char *operation, int imediateV)
-{
-    unsigned int instruction;
-    unsigned int opcode = findIndexOfInstruction(operation);
-    instruction = (opcode) << 24 | imediateV;
+void saveInMemory(int instruction){
+    printf("%d", instruction);
     program_memory[counter] = instruction;
-    //printf("[%s] (%d << 24 | %2d) = 0x%08x\n",operation, opcode, imediateV, program_memory[counter]);
     counter++;
 }
 
 // Stack implementaion
 void push(int x) {
 //printf("-[%4s]-> pushing [%d] onto stack @sp [%d]\n", __func__, x, sp);
+printf("Das wird gepusht %d \n",x);
 stack[sp]=x;
 //printf("-[%4s]-> inc stack pointer [%d -> ", __func__, sp);
 sp++;
@@ -286,4 +208,42 @@ else
 printf("|%7d| %5d |\n", i, stack[i]);
 }
 printf("'-------+--------'\n\n");
+}
+
+
+int main (int argc, char *argv[]) {
+    
+    if(strcmp(argv[1], "--help") == 0)
+    {
+        printf("usage: ./njvm [option] [option] ...\n"
+               "--prog1          select program 1 to execute\n"
+               "--prog2          select program 2 to execute\n"
+               "--prog3          select program 3 to execute\n"
+               "--version        show version and exit\n"
+               "--help           show this help and exit\n");
+        exit(0);
+    }
+    else if(strcmp(argv[1], "--version") == 0)
+    {
+        printf("Ninja Virtual Machine version 1\n");
+        exit(0);
+    }
+    else if (strcmp(argv[1], "--prog1") == 0)
+    {
+
+        start(1);
+    }
+    else if (strcmp(argv[1], "--prog2") == 0)
+    {
+        start(2);
+    }
+    else if (strcmp(argv[1], "--prog3") == 0)
+    {
+        start(3);
+    }
+    else
+    {
+        printf("unknown command line argument '%s', try './njvm --help'\n", argv[1]);
+    }
+    return 0;
 }
---
+++ Aufgabe2/prog4.asm
@@ -0,0 +1,19 @@
+    pushc	2
+	popg	0
+	pushg	0
+	pushc	3
+	add
+	popg	1
+	pushc	7
+	pushg	1
+	mul
+	pushg	0
+	add
+	popg	0
+	pushg	0
+	pushc	-33
+	add
+	wrint
+	pushc	'\n'
+	wrchr
+	halt
